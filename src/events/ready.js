8c4b1b8cc17cccd01d737266f5f67f53dd8c04e9
Азукиов <93555197+Azukiov@users.noreply.github.com>
2023-08-12 21:57:51 +0200

    #011

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,7 @@
 const { Events, EmbedBuilder, WebhookClient } = require('discord.js');
-const { wait, log } = require('../utils/functions.js');
+const { wait, log, dt } = require('../utils/functions.js');
+const fs = require('fs');
+const moment = require('moment');
 
 module.exports = {
     name: Events.ClientReady,
@@ -8,6 +10,15 @@ module.exports = {
         try {
             await wait(2000)
             log(`Logged in as ${client.user.tag}!`);
+
+            // check if logs folder exists and create it if it doesn't
+            if (!fs.existsSync('./logs')) {
+                fs.mkdirSync('./logs');
+            }
+            const date = moment().format('YYYY-MM-DD_HH-mm-ss')
+            const logFile = fs.createWriteStream(`./logs/${date}.log`);
+            logFile.write(`${dt} Logged in as ${client.user.tag}!`);
+
 
         } catch (error) {
             console.error(`Error while logging in as ${client.user.tag}`);
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -5,4 +5,6 @@ const color = "#9968d3";
 const wait = require('node:timers/promises').setTimeout;
 const log = l => console.log(`[${moment().format('DD/MM HH:mm:ss')}] => ${l}`);
 
-module.exports = { wait, log, color }
+const dt = `[${moment().format('YYYY-MM-DD_HH:mm:ss')}] =>`
+
+module.exports = { wait, log, color, dt }
